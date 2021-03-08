ecd1883604ff757712045347af53d89d9e556c95
sosonian <sosonian@gmail.com>
2021-03-08 04:27:30 +0000

    commit code

--- src/DemoApp.js
+++ src/DemoApp.js
@@ -439,7 +439,7 @@ class DemoApp extends Component{
     render(){
        
         console.log("DemoApp render")
-        console.log("v25")
+        console.log("v26")
        
         //console.log(this.state.selectedObjInfo)
         let boxesSetting=[
